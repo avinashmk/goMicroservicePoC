449b7cd44a7ff1a369c37951cbeebe252bcdcaa5
avinashmk <avinashmk@users.noreply.github.com>
2020-02-18 17:45:15 +0100

    Genesis Commit
    something from google

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func setupRouter(router *mux.Router) {
+	router.
+		Methods("POST").
+		Path("/endpoint").
+		HandlerFunc(postFunction)
+}
+
+func postFunction(w http.ResponseWriter, r *http.Request) {
+	log.Println("You called a thing!")
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	setupRouter(router)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
